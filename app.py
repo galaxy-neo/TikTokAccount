2a8ab3e860b38b6d56128045d507175f9faf11ce
neo galaxy <webqart@icloud.com>
2025-07-12 10:42:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device
 # tiktokapi
 tiktok x-ladon, tiktok x-argus, tiktok x-gorgon, tiktok x-khronos, tiktok device register, tiktok api
 
-# TikTok Private API
+# TikTok Private API test
 
 # Tiktok (Source Code)
 * Private API
